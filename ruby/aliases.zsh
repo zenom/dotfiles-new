7b5ec11ba713fe3bf173c067393d0411d3d17284
Andrew Holman <andy@conspyre.com>
2012-06-06 08:42:33 -0400

    update ruby aliases for noglob. add shake command for the rare times I need it

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -13,6 +13,8 @@ alias bp='bundle package'
 alias bout='bundle-outdated'
 alias binit='bi && bp && echo "vendor/bundle" >> .gitignore'
 alias bake='be rake'
+alias shake='noglob bundle exec rake'
 
 alias migratep='RAILS_ENV=production bake db:migrate'
 alias migrate='RAILS_ENV=development bake db:migrate'
+
