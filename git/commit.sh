eaa9bf0b501cdc67034a5f8bafad18a9503222fb
Andy Holman <andy@conspyre.com>
2012-07-02 20:44:55 -0400

    123 123

--- git/commit.sh
+++ git/commit.sh
@@ -3,5 +3,5 @@ echo "What ticket number:"
 read ticket 
 echo "Commit message for ticket:"
 read message
-echo "Committing: $ticket $message"
+echo "Running git add -A && git commit -m $ticket $message"
 git add -A && git commit  -m "$ticket $message"
